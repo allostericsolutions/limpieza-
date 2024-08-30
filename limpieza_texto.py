7daaffb007993b13400d8081856c155a9fb67198
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-30 11:10:59 -0400

    Update limpieza_texto.py

--- limpieza_texto.py
+++ limpieza_texto.py
@@ -3,15 +3,14 @@ import re
 import numpy as np
 
 def limpiar_y_validar(dato):
-    # Eliminar todos los caracteres no numéricos
+    # Elimine todos los caracteres no numéricos
     dato_limpio = re.sub(r'\D', '', dato).strip()
+
+    # Cuente el número total de dígitos en la celda original
+    total_digitos = len(dato_limpio)
     
-    # Contar número de dígitos en la celda original
-    total_digitos = len(re.sub(r'\D', '', dato))
-    
-    # Verificar si la celda tiene más de 11 dígitos
+    # Si hay más de 11 dígitos, considere cada segmento separado por espacios como un número individual
     if total_digitos > 11:
-        # Considerar cada segmento separado por espacios como un número individual
         numeros_separados = re.split(r'\s+', dato)
         resultados_separados = []
         for num in numeros_separados:
@@ -20,7 +19,7 @@ def limpiar_y_validar(dato):
                 resultados_separados.append(numero_limpio)
         return resultados_separados if resultados_separados else None
     else:
-        # Regla general: Normalizar el número
+        # Regla general: normalizar el número
         if len(dato_limpio) == 10:
             return [dato_limpio]
         return None
@@ -28,8 +27,11 @@ def limpiar_y_validar(dato):
 def procesar_chunk(chunk, output, invalid_numbers_less_than_10, invalid_numbers_greater_than_10):
     fondos_planos = chunk.values.flatten().astype(str).tolist()
     for line in fondos_planos:
-        total_digitos_en_linea = sum(len(re.sub(r'\D', '', number)) for number in re.split(r'\s+', line))
-        numbers = re.split(r'[,\s]+', line)
+        line_digitos = re.sub(r'\D', '', line)
+        if len(line_digitos) > 11:
+            numbers = re.split(r'\s+', line)
+        else:
+            numbers = [line]
         for number in numbers:
             resultados = limpiar_y_validar(number)
             if resultados:
@@ -39,7 +41,7 @@ def procesar_chunk(chunk, output, invalid_numbers_less_than_10, invalid_numbers_
                 num_digits = len(re.sub(r'\D', '', number))
                 if num_digits < 10:
                     invalid_numbers_less_than_10 += 1
-                elif num_digits > 10 and total_digitos_en_linea <= 11:
+                elif num_digits > 10:
                     invalid_numbers_greater_than_10 += 1
     return invalid_numbers_less_than_10, invalid_numbers_greater_than_10
 
@@ -49,67 +51,34 @@ def limpiar_y_procesar_archivo(uploaded_file, file_extension, chunk_size=10000):
     invalid_numbers_greater_than_10 = 0
     output = set()
 
-    if file_extension == "csv":
+    if file_extension in ["csv", "txt"]:
         codificaciones = ['utf-8', 'latin1', 'cp1252']
         for encoding in codificaciones:
             try:
                 uploaded_file.seek(0)
-                reader = pd.read_csv(uploaded_file, chunksize=chunk_size, header=None, encoding=encoding)
+                if file_extension == "csv":
+                    reader = pd.read_csv(uploaded_file, chunksize=chunk_size, header=None, encoding=encoding)
+                elif file_extension == "txt":
+                    reader = (line.decode(encoding).strip() for line in uploaded_file)
                 for chunk_num, chunk in enumerate(reader):
-                    total_numbers += chunk.size
+                    total_numbers += chunk.size if file_extension == "csv" else 1
+                    chunk_df = pd.DataFrame([chunk]) if file_extension == "txt" else chunk
                     invalid_numbers_less_than_10, invalid_numbers_greater_than_10 = procesar_chunk(
-                        chunk, output, invalid_numbers_less_than_10, invalid_numbers_greater_than_10
-                    )
-                break  # Salir del bucle si la codificación tiene éxito
-            except UnicodeDecodeError:
-                continue
-
-    elif file_extension in ["xls", "xlsx"]:
-        codificaciones = ['utf-8', 'latin1', 'cp1252']
-        for encoding in codificaciones:
-            try:
-                uploaded_file.seek(0)
-                if file_extension == "xlsx":
-                    reader = pd.read_excel(uploaded_file, engine='openpyxl', None) 
-                else:
-                    reader = pd.read_excel(uploaded_file, engine='xlrd', None)
-                for sheet_name, sheet in reader.items():
-                    num_chunks = max(1, len(sheet) // chunk_size)
-                    for chunk_num, chunk in enumerate(np.array_split(sheet, num_chunks)):
-                        total_numbers += chunk.size
-                        invalid_numbers_less_than_10, invalid_numbers_greater_than_10 = procesar_chunk(
-                            chunk, output, invalid_numbers_less_than_10, invalid_numbers_greater_than_10
-                        )
+                        chunk_df, output, invalid_numbers_less_than_10, invalid_numbers_greater_than_10)
                 break
             except UnicodeDecodeError:
                 continue
 
-    elif file_extension == "txt":
-        codificaciones = ['utf-8', 'latin1', 'cp1252']
-        for encoding in codificaciones:
-            try:
-                uploaded_file.seek(0)
-                total_lines = sum(1 for _ in uploaded_file)
-                uploaded_file.seek(0)
-                for i, line in enumerate(uploaded_file):
-                    cleaned_line = line.decode(encoding).strip()
-                    numbers = re.split(r'[,\s]+', cleaned_line)
-                    for number in numbers:
-                        total_numbers += 1
-                        resultados = limpiar_y_validar(number)
-                        if resultados:
-                            for cleaned_number in resultados:
-                                output.add(cleaned_number)
-                        else:
-                            num_digits = len(re.sub(r'\D', '', number))
-                            if num_digits < 10:
-                                invalid_numbers_less_than_10 += 1
-                            elif num_digits > 10 and total_digitos_en_linea <= 11:
-                                invalid_numbers_greater_than_10 += 1
-
-                break  # Salir del bucle si la codificación tiene éxito
-            except UnicodeDecodeError:
-                continue
+    elif file_extension in ["xls", "xlsx"]:
+        engine = 'openpyxl' if file_extension == "xlsx" else 'xlrd'
+        reader = pd.read_excel(uploaded_file, engine=engine, sheet_name=None)
+        for sheet_name, sheet in reader.items():
+            num_chunks = max(1, len(sheet) // chunk_size)
+            chunks = np.array_split(sheet, num_chunks)
+            for chunk in chunks:
+                total_numbers += len(chunk)
+                invalid_numbers_less_than_10, invalid_numbers_greater_than_10 = procesar_chunk(
+                    chunk, output, invalid_numbers_less_than_10, invalid_numbers_greater_than_10)
 
     else:
         raise ValueError("Invalid file format. Please upload a CSV, Excel, or Text file.")
