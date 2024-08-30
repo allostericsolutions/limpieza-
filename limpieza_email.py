1f913efbdcc2fbfec77f25b755ac4b72acd146d8
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-30 15:06:33 -0400

    Update limpieza_email.py

--- limpieza_email.py
+++ limpieza_email.py
@@ -1,1 +1,11 @@
+import re
 
+def validar_email(email):
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(email_regex, email) is not None
+
+def limpiar_y_validar_correo(dato):
+    dato = dato.strip().lower()  # Convertir a minúsculas
+    if validar_email(dato):
+        return dato
+    return None
