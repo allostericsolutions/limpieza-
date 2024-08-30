6015a045fdeb52a58f1e4db32f5e0a35579e7a28
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-30 15:07:46 -0400

    Update limpieza.py

--- limpieza.py
+++ limpieza.py
@@ -6,7 +6,8 @@ import os
 from fpdf import FPDF
 import tempfile
 import numpy as np
-from limpieza_texto import limpiar_y_procesar_archivo  # Importar la función desde el módulo
+from limpieza_texto import limpiar_y_procesar_archivo
+from limpieza_email import limpiar_y_validar_correo  # Importar la función desde el módulo
 from contenido import mostrar_features, mostrar_como_usar  # Importar las nuevas funciones
 
 def generar_pdf_reporte(dataframe, info_df):
@@ -34,16 +35,31 @@ def generar_pdf(dataframe):
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200, 10, txt="Cleaned Phone Numbers", ln=True, align='C')
+    pdf.cell(200, 10, txt="Cleaned Data", ln=True, align='C')
 
     for index, row in dataframe.iterrows():
-        pdf.cell(200, 10, txt=row['cleaned_numbers'], ln=True, align='C')
+        pdf.cell(200, 10, txt=row['cleaned_data'], ln=True, align='C')
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
         pdf.output(tmpfile.name)
 
     return tmpfile.name
 
+def process_chunk(chunk, output, tipo):
+    fondos_planos = chunk.values.flatten().astype(str).tolist()
+    for line in fondos_planos:
+        process_line(line, output, tipo)
+
+def process_line(line, output, tipo):
+    datos = re.split(r'[,\s]+', line.strip())
+    for dato in datos:
+        if tipo == 'emails':
+            cleaned_dato = limpiar_y_validar_correo(dato)
+        else:
+            cleaned_dato = limpiar_y_validar(dato)
+        if cleaned_dato is not None:
+            output.add(cleaned_dato)
+            
 def main():
     st.title("Dr. Cleaner")
 
@@ -56,86 +72,58 @@ def main():
     mostrar_features()
     mostrar_como_usar()
 
+    options = st.radio(
+        "Select Data Type:",
+        ('Phone Numbers', 'Emails'))
+
     uploaded_files = st.file_uploader("Drop Your Junk Here", type=["csv", "xls", "xlsx", "txt"], accept_multiple_files=True)
 
-    if uploaded_files is not None:
-        total_data = []
-        total_numbers = 0
-        invalid_numbers_less_than_10 = 0
-        invalid_numbers_greater_than_10 = 0
-        output = set()
+    if uploaded_files:
+        tipo = 'emails' if options == 'Emails' else 'telefonos'
+        output, invalid_items, total_items = procesar_archivos(uploaded_files, tipo)
 
-        for uploaded_file in uploaded_files:
-            file_extension = uploaded_file.name.split('.')[-1]
-
-            # Llamar la función de limpieza y procesamiento para cada archivo
-            resultado = limpiar_y_procesar_archivo(uploaded_file, file_extension)
-
-            total_data.extend(resultado["cleaned_numbers"])
-            total_numbers += resultado["total_numbers"]
-            invalid_numbers_less_than_10 += resultado["invalid_numbers_less_than_10"]
-            invalid_numbers_greater_than_10 += resultado["invalid_numbers_greater_than_10"]
-
-            output.update(resultado["cleaned_numbers"])
-
-        df = pd.DataFrame()
-        df["cleaned_numbers"] = list(output)
-        
+        df = pd.DataFrame({'cleaned_data': list(output)})
         st.write("Cleaned Data:")
         st.dataframe(df)
 
-        formato_salida = st.selectbox("Choose Your Fancy Output!", ["Excel", "PDF", "CSV"])
-
-        if formato_salida == "Excel":
-            buffer = BytesIO()
-            df.to_excel(buffer, index=False, header=False)  # Exportar a Excel sin encabezado
-            buffer.seek(0)
-
+        formato_salida = st.selectbox("Choose Your Fancy Output!", ["CSV", "Excel", "PDF"])
+        
+        if formato_salida == "CSV":
+            buffer = download_csv(df)
             st.download_button(
                 label="Grab your things here",
                 data=buffer.getvalue(),
-                file_name='cleaned_numbers.xlsx',
-                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                file_name='cleaned_data.csv',
+                mime='text/csv'
             )
-
+        elif formato_salida == "Excel":
+            buffer = download_excel(df)
+            if buffer:
+                st.download_button(
+                    label="Grab your things here",
+                    data=buffer.getvalue(),
+                    file_name='cleaned_data.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )
         elif formato_salida == "PDF":
             pdf_file_path = generar_pdf(df)
             with open(pdf_file_path, "rb") as pdf_file:
                 st.download_button(
                     label="Download Cleaned PDF",
                     data=pdf_file,
-                    file_name='cleaned_numbers.pdf',
+                    file_name='cleaned_data.pdf',
                     mime='application/pdf'
                 )
             os.remove(pdf_file_path)
 
-        elif formato_salida == "CSV":
-            buffer = BytesIO()
-            df.to_csv(buffer, index=False, header=False)  # Exportar a CSV sin encabezado
-            buffer.seek(0)
-
-            st.download_button(
-                label="Download Cleaned CSV",
-                data=buffer.getvalue(),
-                file_name='cleaned_numbers.csv',
-                mime='text/csv'
-            )
-
-        n_validos = len(output)
-        n_duplicados_eliminados = total_numbers - (
-            n_validos + invalid_numbers_less_than_10 + invalid_numbers_greater_than_10
-        )
-
         reporte = {
-            'Total numbers found in the file(s)': total_numbers,
-            'Total valid numbers processed (exactly 10 digits)': n_validos,
-            'Total disqualified numbers (less than 10 digits)': invalid_numbers_less_than_10,
-            'Total disqualified numbers (more than 10 digits)': invalid_numbers_greater_than_10,
-            'Total duplicate numbers removed': n_duplicados_eliminados
+            'Total items found in the file': total_items,
+            'Total valid items processed': len(output),
+            'Total invalid items': invalid_items,
+            'Total duplicate items removed': total_items - len(output) - invalid_items
         }
 
         reporte_df = pd.DataFrame(list(reporte.items()), columns=['Description', 'Count'])
-
         info_df = pd.DataFrame({
             'Analysis': ['Analyzed by Allosteric Solutions', 'www.allostericsolutions.com', 'franciscocuriel@allostericsolutions.com']
         })
@@ -147,18 +135,19 @@ def main():
 
         if formato_reporte == "Excel":
             reporte_file = 'report.xlsx'
-            with pd.ExcelWriter(reporte_file) as writer:
-                reporte_df.to_excel(writer, sheet_name='Summary', index=False)
-                info_df.to_excel(writer, sheet_name='Analysis Info', index=False)
-
-            with open(reporte_file, "rb") as report_file:
-                st.download_button(
-                    label="Retrieve Your Report Here, Hurry Up man!",
-                    data=report_file,
-                    file_name='report.xlsx',
-                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                )
-
+            try:
+                with pd.ExcelWriter(reporte_file, engine='xlsxwriter') as writer:
+                    reporte_df.to_excel(writer, sheet_name='Summary', index=False)
+                    info_df.to_excel(writer, sheet_name='Analysis Info', index=False)
+                with open(reporte_file, "rb") as report_file:
+                    st.download_button(
+                        label="Retrieve Your Report Here",
+                        data=report_file,
+                        file_name='report.xlsx',
+                        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                    )
+            except Exception as e:
+                st.error(f"Error exporting report to Excel: {e}")
         elif formato_reporte == "PDF":
             pdf_report_file_path = generar_pdf_reporte(reporte_df, info_df)
             with open(pdf_report_file_path, "rb") as pdf_report_file:
