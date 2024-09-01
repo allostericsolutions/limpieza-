4e1ded908b841d9072d8d7b92175d421b7680785
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-09-01 18:30:49 -0400

    Update limpieza.py

--- limpieza.py
+++ limpieza.py
@@ -156,95 +156,95 @@ def main():
 
     uploaded_files = st.file_uploader("Drop Your Junk Here", type=["csv", "xls", "xlsx", "txt"], accept_multiple_files=True)
     
-    if uploaded_files:
+    if options == 'Bear Trap':
+        st.write("You've triggered the bear trap! 🪤")
+        st.image("out-0 (1).png", width=360)
+    elif options == 'Meet the Genius Behind the Text Cleaning App':
+        st.write("The creator of the innovative text cleaning app is a highly accomplished programmer who studied at the prestigious Squirrel University of Colorado. With a strong foundation in computer science, he has made significant contributions to the field of programming. His expertise extends beyond app development; he has also worked on high-profile projects for NASA, including the development of software for space probes and rovers. In addition to his professional achievements, he is an avid collector of nuts and a fervent admirer of the band Supertramp. His dedication to excellence and his impressive track record make him a standout figure in the tech community.!")
+        st.image("output_file_1.png", width=360)# Usar imagen out-0 (1).png aquí
+    elif uploaded_files:
         tipo = 'emails' if options == 'Emails' else 'telefonos'
         if options == 'Postal Codes':
             tipo = 'codigos_postales'
-        if options == 'Bear Trap':
-            st.write("You've triggered the bear trap! 🪤")
-            st.image("out-0 (1).png", width=360)
-        elif options == 'Meet the Genius Behind the Text Cleaning App':
-            st.write("The creator of the innovative text cleaning app is a highly accomplished programmer who studied at the prestigious Squirrel University of Colorado. With a strong foundation in computer science, he has made significant contributions to the field of programming. His expertise extends beyond app development; he has also worked on high-profile projects for NASA, including the development of software for space probes and rovers. In addition to his professional achievements, he is an avid collector of nuts and a fervent admirer of the band Supertramp. His dedication to excellence and his impressive track record make him a standout figure in the tech community.!")
-            st.image("output_file_1.png", width=360)# Usar imagen out-0 (1).png aquí
-        else:
-            output, invalid_items, total_items = procesar_archivos(uploaded_files, tipo)
+        
+        output, invalid_items, total_items = procesar_archivos(uploaded_files, tipo)
 
-            df = pd.DataFrame({'cleaned_data': list(output)})
-            st.write("Cleaned Data:")
-            st.dataframe(df)
+        df = pd.DataFrame({'cleaned_data': list(output)})
+        st.write("Cleaned Data:")
+        st.dataframe(df)
 
-            formato_salida = st.selectbox("Choose Your Fancy Polished Output Format!", ["CSV", "Excel", "PDF"])
-            
-            if formato_salida == "CSV":
-                buffer = download_csv(df)
+        formato_salida = st.selectbox("Choose Your Fancy Polished Output Format!", ["CSV", "Excel", "PDF"])
+        
+        if formato_salida == "CSV":
+            buffer = download_csv(df)
+            st.download_button(
+                label="Grab your things here",
+                data=buffer.getvalue(),
+                file_name='cleaned_data.csv',
+                mime='text/csv'
+            )
+        elif formato_salida == "Excel":
+            buffer = download_excel(df)
+            if buffer:
                 st.download_button(
                     label="Grab your things here",
                     data=buffer.getvalue(),
-                    file_name='cleaned_data.csv',
-                    mime='text/csv'
+                    file_name='cleaned_data.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                 )
-            elif formato_salida == "Excel":
-                buffer = download_excel(df)
-                if buffer:
+        elif formato_salida == "PDF":
+            pdf_file_path = generar_pdf(df)
+            with open(pdf_file_path, "rb") as pdf_file:
+                st.download_button(
+                    label="Download Cleaned PDF",
+                    data=pdf_file,
+                    file_name='cleaned_data.pdf',
+                    mime='application/pdf'
+                )
+            os.remove(pdf_file_path)
+
+        reporte = {
+            'Total items found in the file': total_items,
+            'Total valid items processed': len(output),
+            'Total invalid items': invalid_items,
+            'Total duplicate items removed': total_items - len(output) - invalid_items
+        }
+
+        reporte_df = pd.DataFrame(list(reporte.items()), columns=['Description', 'Count'])
+        info_df = pd.DataFrame({
+            'Analysis': ['Analyzed by Allosteric Solutions', 'www.allostericsolutions.com', 'franciscocuriel@allostericsolutions.com']
+        })
+
+        st.write("Report Summary (you're welcome):")
+        st.dataframe(reporte_df)
+
+        formato_reporte = st.selectbox("Select report format", ["Excel", "PDF"])
+
+        if formato_reporte == "Excel":
+            reporte_file = 'report.xlsx'
+            try:
+                with pd.ExcelWriter(reporte_file, engine='xlsxwriter') as writer:
+                    reporte_df.to_excel(writer, sheet_name='Summary', index=False)
+                    info_df.to_excel(writer, sheet_name='Analysis Info', index=False)
+                with open(reporte_file, "rb") as report_file:
                     st.download_button(
-                        label="Grab your things here",
-                        data=buffer.getvalue(),
-                        file_name='cleaned_data.xlsx',
+                        label="Retrieve Your Report Here",
+                        data=report_file,
+                        file_name='report.xlsx',
                         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                     )
-            elif formato_salida == "PDF":
-                pdf_file_path = generar_pdf(df)
-                with open(pdf_file_path, "rb") as pdf_file:
-                    st.download_button(
-                        label="Download Cleaned PDF",
-                        data=pdf_file,
-                        file_name='cleaned_data.pdf',
-                        mime='application/pdf'
-                    )
-                os.remove(pdf_file_path)
-
-            reporte = {
-                'Total items found in the file': total_items,
-                'Total valid items processed': len(output),
-                'Total invalid items': invalid_items,
-                'Total duplicate items removed': total_items - len(output) - invalid_items
-            }
-
-            reporte_df = pd.DataFrame(list(reporte.items()), columns=['Description', 'Count'])
-            info_df = pd.DataFrame({
-                'Analysis': ['Analyzed by Allosteric Solutions', 'www.allostericsolutions.com', 'franciscocuriel@allostericsolutions.com']
-            })
-
-            st.write("Report Summary (you're welcome):")
-            st.dataframe(reporte_df)
-
-            formato_reporte = st.selectbox("Select report format", ["Excel", "PDF"])
-
-            if formato_reporte == "Excel":
-                reporte_file = 'report.xlsx'
-                try:
-                    with pd.ExcelWriter(reporte_file, engine='xlsxwriter') as writer:
-                        reporte_df.to_excel(writer, sheet_name='Summary', index=False)
-                        info_df.to_excel(writer, sheet_name='Analysis Info', index=False)
-                    with open(reporte_file, "rb") as report_file:
-                        st.download_button(
-                            label="Retrieve Your Report Here",
-                            data=report_file,
-                            file_name='report.xlsx',
-                            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                        )
-                except Exception as e:
-                    st.error(f"Error exporting report to Excel: {e}")
-            elif formato_reporte == "PDF":
-                pdf_report_file_path = generar_pdf_reporte(reporte_df, info_df)
-                with open(pdf_report_file_path, "rb") as pdf_report_file:
-                    st.download_button(
-                        label="Download Report (PDF)",
-                        data=pdf_report_file,
-                        file_name='report.pdf',
-                        mime='application/pdf'
-                    )
-                os.remove(pdf_report_file_path)
+            except Exception as e:
+                st.error(f"Error exporting report to Excel: {e}")
+        elif formato_reporte == "PDF":
+            pdf_report_file_path = generar_pdf_reporte(reporte_df, info_df)
+            with open(pdf_report_file_path, "rb") as pdf_report_file:
+                st.download_button(
+                    label="Download Report (PDF)",
+                    data=pdf_report_file,
+                    file_name='report.pdf',
+                    mime='application/pdf'
+                )
+            os.remove(pdf_report_file_path)
 
 if __name__ == "__main__":
     main()
